09d594ebc6b3743eed6eae6d06fc47f5f7887455
Carlos Henrryque <kaike.ch03@gmail.com>
2024-03-21 15:07:18 -0300

    Exercíco de teste unitários EBAC

--- multiplos.js
+++ multiplos.js
@@ -10,6 +10,9 @@ function somaMultiplosDe5ou7() {
     return soma;
 }
 
+// Exportando a função somaMultiplosDe5ou7
+module.exports = somaMultiplosDe5ou7;
+
 // Exemplo de uso:
 const resultado = somaMultiplosDe5ou7();
 console.log("A soma dos múltiplos de 5 ou 7 abaixo de 1000 é:", resultado);
